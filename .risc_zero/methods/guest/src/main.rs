6bea6337c7ce1995ca095c0e2a07cf256c97535d
PatStiles <pdstiles78@gmail.com>
2024-07-04 20:40:30 -0700

    rm code in .risc_zero main

--- .risc_zero/methods/guest/src/main.rs
+++ .risc_zero/methods/guest/src/main.rs
@@ -1,14 +1,3 @@
 #![no_main]
 
 risc0_zkvm::guest::entry!(main);
-fn main() {
-    let n = 10u32;
-    let mut a: u32 = 0;
-    let mut b: u32 = 1;
-    for _ in 0..n {
-        let mut c = a + b;
-        c %= 7919; // Modulus to prevent overflow.
-        a = b;
-        b = c;
-    }
-}
