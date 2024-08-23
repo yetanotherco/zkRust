0eab31567ca7ef658051f046a65b8501d0a1d10c
PatStiles <pdstiles78@gmail.com>
2024-08-23 00:40:43 -0500

    nits

--- src/sp1.rs
+++ src/sp1.rs
@@ -18,7 +18,7 @@ pub const SP1_PROOF_PATH: &str = "./proof_data/sp1/sp1.proof";
 pub const SP1_PROGRAM_HEADER: &str = "#![no_main]\nsp1_zkvm::entrypoint!(main);\n";
 
 /// SP1 Cargo patch for accelerated SHA-256, K256, and bigint-multiplication circuits
-pub const SP1_ACCELERATION_IMPORT: &str = "\n[patch.crates-io]\nsha2-v0-10-8 = { git = \"https://github.com/sp1-patches/RustCrypto-hashes\", package = \"sha2\", branch = \"patch-sha2-v0.10.8\" }\nsha3-v0-10-8 = { git = \"https://github.com/sp1-patches/RustCrypto-hashes\", package = \"sha3\", branch = \"patch-sha3-v0.10.8\" }\ncrypto-bigint = { git = \"https://github.com/sp1-patches/RustCrypto-bigint\", branch = \"patch-v0.5.5\" }\ntiny-keccak = { git = \"https://github.com/sp1-patches/tiny-keccak\", branch = \"patch-v2.0.2\" }\ned25519-consensus = { git = \"https://github.com/sp1-patches/ed25519-consensus\", branch = \"patch-v2.1.0\" }\necdsa-core = { git = \"https://github.com/sp1-patches/signatures\", package = \"ecdsa\", branch = \"patch-ecdsa-v0.16.9\" }\n";
+pub const SP1_ACCELERATION_IMPORT: &str = "\n[patch.crates-io]\nsha2 = { git = \"https://github.com/sp1-patches/RustCrypto-hashes\", package = \"sha2\", branch = \"patch-sha2-v0.10.6\" }\nsha3 = { git = \"https://github.com/sp1-patches/RustCrypto-hashes\", package = \"sha3\", branch = \"patch-sha3-v0.10.8\" }\ncrypto-bigint = { git = \"https://github.com/sp1-patches/RustCrypto-bigint\", branch = \"patch-v0.5.5\" }\ntiny-keccak = { git = \"https://github.com/sp1-patches/tiny-keccak\", branch = \"patch-v2.0.2\" }\ned25519-consensus = { git = \"https://github.com/sp1-patches/ed25519-consensus\", branch = \"patch-v2.1.0\" }\necdsa-core = { git = \"https://github.com/sp1-patches/signatures\", package = \"ecdsa\", branch = \"patch-ecdsa-v0.16.9\" }\n";
 
 /// This function mainly adds this header to the guest in order for it to be proven by
 /// sp1:
@@ -34,11 +34,13 @@ pub fn prepare_sp1_program() -> io::Result<()> {
 /// Generates SP1 proof and ELF
 pub fn generate_sp1_proof() -> io::Result<()> {
     let guest_path = fs::canonicalize(SP1_SCRIPT_DIR)?;
+
     Command::new("cargo")
         .arg("run")
         .arg("--release")
         .current_dir(guest_path)
-        .status()?;
+        .status()
+        .unwrap();
 
     Ok(())
 }
