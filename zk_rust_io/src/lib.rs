f2af8cdff99602d67b5b41d8743ea61cace383e2
PatStiles <pdstiles78@gmail.com>
2024-08-26 18:10:51 -0500

    make commit default

--- zk_rust_io/src/lib.rs
+++ zk_rust_io/src/lib.rs
@@ -1,11 +1,19 @@
 use serde::{de::DeserializeOwned, Serialize};
 
-//TODO: How to handle read case
 #[inline(never)]
-pub fn read<T: DeserializeOwned>() -> T { unimplemented!("ZK Rust funcion `io::commit()` is a compile time symbol" ) }
+pub fn read<T: DeserializeOwned + Default>() -> T {
+    println!("ZK Rust funcion `io::commit()` is a compile time symbol");
+    T::default()
+}
 #[inline(never)]
-pub fn commit<T: Serialize>(_value: &T) { println!("ZK Rust function `io::commit()` is a compile time symbol") }
+pub fn commit<T: Serialize>(_value: &T) {
+    println!("ZK Rust function `io::commit()` is a compile time symbol")
+}
 #[inline(never)]
-pub fn write(_buf: &[u8]) { println!("ZK Rust function `io::write() is a compile time symbol") }
+pub fn write(_buf: &[u8]) {
+    println!("ZK Rust function `io::write() is a compile time symbol")
+}
 #[inline(never)]
-pub fn out() { println!("Zk Rust function `io::out()` is a compile time symbol") }
+pub fn out() {
+    println!("Zk Rust function `io::out()` is a compile time symbol")
+}
