066ee68a3cb701a425b413a720414aa7e47ac154
PatStiles <pdstiles78@gmail.com>
2024-08-26 20:41:33 -0500

    nit comment

--- zk_rust_io/src/lib.rs
+++ zk_rust_io/src/lib.rs
@@ -2,7 +2,7 @@ use serde::{de::DeserializeOwned, Serialize};
 
 #[inline(never)]
 pub fn read<T: DeserializeOwned + Default>() -> T {
-    println!("ZK Rust funcion `io::commit()` is a compile time symbol");
+    println!("ZK Rust function `io::commit()` is a compile time symbol");
     T::default()
 }
 #[inline(never)]
