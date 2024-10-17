696bc60b9a6b219dad256fc7b2030564a90f648f
PatStiles <pdstiles78@gmail.com>
2024-10-16 22:58:09 -0300

    nit

--- examples/fibonacci/src/main.rs
+++ examples/fibonacci/src/main.rs
@@ -23,9 +23,9 @@ fn input() {
 }
 
 fn output() {
-    let (a, b, c): (u32, u32, u32) = zk_rust_io::out();
+    let (n, a, b): (u32, u32, u32) = zk_rust_io::out();
 
+    println!("n: {}", n);
     println!("a: {}", a);
     println!("b: {}", b);
-    println!("c: {}", c);
 }
